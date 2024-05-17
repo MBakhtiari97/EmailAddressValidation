c79f70ecdec723303ec00a072b5eb1c32288fd40
Mohammad Bakhtiari <mohammadba86@yahoo.com>
2024-05-17 15:38:03 +0330

    Add project files.

---
+++ EAV/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Mail;
+using System.Text.RegularExpressions;
+public class Program
+{
+    public static void Main()
+    {
+        var invalidEmail = "test.com";
+        var validEmail = "test@gmail.com";
+
+        if (ValidateEmail(invalidEmail))
+            Console.WriteLine($"{invalidEmail} Is Valid");
+        else
+            Console.WriteLine($"{invalidEmail} Is Invalid");
+
+        if (ValidateEmail(validEmail))
+            Console.WriteLine($"{validEmail} Is Valid");
+        else
+            Console.WriteLine($"{validEmail} Is Invalid");
+    }
+
+    //First Approach
+    public static bool ValidateEmail(string emailAddress)
+    {
+        string regexPattern = @"^[a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$";
+        Regex regex = new Regex(regexPattern);
+        return regex.IsMatch(emailAddress);
+    }
+
+    //Second Approach
+    //public static bool ValidateEmail(string emailAddress)
+    //{
+    //    if (MailAddress.TryCreate(emailAddress, out var res))
+    //        return true;
+    //    return false;
+    //}
+}
